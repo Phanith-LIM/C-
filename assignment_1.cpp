a62cbaae18e8ab8e98a515193d8d8603b6015fb2
Lim Phanith <limphanith.office@gmail.com>
2022-08-11 19:02:54 +0700

    add code

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
 int main(){
-    std::cout << "Hello World" <<std::endl;
+    string number;
+    float average;
+    float sum = 0;
+    char decimals_digit[] = "0123456789";
+    bool isDecimal = true;
+    short int sizeNumber = sizeof(number) / sizeof(number[0]);
+    cout << "Enter numer : ";
+    getline(cin, number);
+    
+    //check number isDecimal
+    for(int i = 0; i < number.size(); i++){
+        if(strchr(decimals_digit, number[i]) == NULL){
+            isDecimal = false;
+            cout << "This is not decimal number "<<endl;
+                break;
+        }
+    }
+    if (isDecimal){
+        for(int i = 0; i < number.size(); i++){
+            sum = sum  + (number[i] - 48);
+        }
+        average = sum / number.size();
+        cout << "The average of the digits in "<< number << " is "<< average <<endl;
+    }
     return 0;
 }
