65fbaf72ec65c1aa08e0675618508708264b7d7e
Lim Phanith <limphanith.office@gmail.com>
2022-08-12 13:59:22 +0700

    Done!

--- excercises_3.cpp
+++ excercises_3.cpp
@@ -6,55 +6,49 @@ int main(){
     std::cout << "Enter your password : ";
     char rules[] = "0123456789";
     getline(std::cin, password);
-    bool isPass1 = true;
-    bool isPass2 = true;
-    bool isPass3;
-    bool isPass4;
-    bool isPass5;
+    bool isPassed1 = password.length() >= 8;
+    bool isPassed2;
+    bool isPassed3;
+    bool isPassed4;
+    bool isPassed5;
     //isTest1 : A password must have exactly 8 characters
-    if(password.length() >= 8){
+    if (isPassed1){
         std::cout << "\x1b[32m Passed Test 1 : ✅" << std::endl;
         for (int i = 0; i < password.length(); i++)
         {
             //isTest2 : A password must consist of only digits and letters.
             if ((std::all_of(password.begin(), password.end(), isalpha) || (std::all_of(password.begin(), password.end(), isalnum)))){
-                isPass2 = true;
+                isPassed2 = true;
             }
-            else isPass2 = false;
+            else isPassed2 = false;
 
             //isTest3 : A password must always start with a digit.
-            if(strchr(rules,password[0])) isPass3 = true;
-            else isPass3 = false;
+            if(strchr(rules,password[0])) isPassed3 = true;
+            else isPassed3 = false;
             
             //isTest4 : A password must contain at least one uppercase letter.
             if (isupper(password[i])){
-                isPass4 = true;
+                isPassed4 = true;
                 break;
-            }else isPass4 = false;
+            }else isPassed4 = false;
     
         }
-        if (isPass2)
-            std::cout << "\x1b[32m Passed Test 2 : ✅" << std::endl;
-        else
-             std::cout << "\x1b[31m Failed Test 2 : ❌" <<std::endl;
+        //Display isPassed rules for passwords
+        if (isPassed2) std::cout << "\x1b[32m Passed Test 2 : ✅" << std::endl;
+        else std::cout << "\x1b[31m Failed Test 2 : ❌" <<std::endl;
 
-        if (isPass3)
-            std::cout << "\x1b[32m Passed Test 3 : ✅" << std::endl;
-        else
-             std::cout << "\x1b[31m Failed Test 3 : ❌" <<std::endl;
-        
-         if (isPass4)
-            std::cout << "\x1b[32m Passed Test 4 : ✅" << std::endl;
-        else
-             std::cout << "\x1b[31m Failed Test 4 : ❌" <<std::endl;
+        if (isPassed3) std::cout << "\x1b[32m Passed Test 3 : ✅" << std::endl;     
+        else std::cout << "\x1b[31m Failed Test 3 : ❌" <<std::endl;
+             
+        if (isPassed4) std::cout << "\x1b[32m Passed Test 4 : ✅" << std::endl;   
+        else std::cout << "\x1b[31m Failed Test 4 : ❌" <<std::endl;
     }else{
-        isPass1 = false;
+        isPassed1 = false;
         std::cout << "\x1b[31m Failed Test 1 : ❌" <<std::endl;
     }
-
     //Final evaluate
-    isPass5 = isPass1 && isPass2 && isPass3 && isPass4;
-    if(isPass5){
+    isPassed5 = isPassed1 && isPassed2 && isPassed3 && isPassed4;
+    if(isPassed5){
         std::cout << "------------------------------" << std::endl;
         std::cout << "\x1b[32m Passed Final Test : ✅" << std::endl;
         std::cout << "\x1b[32m Valid Password" << std::endl;
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+for (int i = 0; i < password.length(); i++){
+                        if (isupper(password[i])){
+                            break;
+                        }else{
+                            isPass4 = false;
+                        }
+                    }
+                }
