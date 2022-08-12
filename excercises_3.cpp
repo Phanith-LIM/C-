137778ac7add0d7c0387d8576ca6c8088bf35af9
Lim Phanith <limphanith.office@gmail.com>
2022-08-12 13:47:37 +0700

    update code

--- excercises_3.cpp
+++ excercises_3.cpp
@@ -8,33 +8,31 @@ int main(){
     getline(std::cin, password);
     bool isPass1 = true;
     bool isPass2 = true;
-    bool isPass3 = true;
-    bool isPass4 = true;
+    bool isPass3;
+    bool isPass4;
     bool isPass5;
     //isTest1 : A password must have exactly 8 characters
     if(password.length() >= 8){
         std::cout << "\x1b[32m Passed Test 1 : ✅" << std::endl;
-        //isTest2 : A password must consist of only digits and letters.
+        for (int i = 0; i < password.length(); i++)
+        {
+            //isTest2 : A password must consist of only digits and letters.
             if ((std::all_of(password.begin(), password.end(), isalpha) || (std::all_of(password.begin(), password.end(), isalnum)))){
-                //isTest3 : A password must always start with a digit.
-                if(strchr(rules,password[0])){
-                    //isTest4 : A password must contain at least one uppercase letter.
-                    for (int i = 0; i < password.length(); i++){
-                        if (isupper(password[i])){
-                            isPass4 = true;
-                            break;
-                        }else{
-                            isPass4 = false;
-                        }
-                    }
-                }
-                else{
-                    isPass3 = false;  
-                }
+                isPass2 = true;
             }
-            else{
-                isPass2 = false;
-            }
+            else isPass2 = false;
+
+            //isTest3 : A password must always start with a digit.
+            if(strchr(rules,password[0])) isPass3 = true;
+            else isPass3 = false;
+            
+            //isTest4 : A password must contain at least one uppercase letter.
+            if (isupper(password[i])){
+                isPass4 = true;
+                break;
+            }else isPass4 = false;
+    
+        }
         if (isPass2)
             std::cout << "\x1b[32m Passed Test 2 : ✅" << std::endl;
         else
@@ -57,11 +55,13 @@ int main(){
     //Final evaluate
     isPass5 = isPass1 && isPass2 && isPass3 && isPass4;
     if(isPass5){
+        std::cout << "------------------------------" << std::endl;
         std::cout << "\x1b[32m Passed Final Test : ✅" << std::endl;
         std::cout << "\x1b[32m Valid Password" << std::endl;
     }else{
+        std::cout << "------------------------------" << std::endl;
         std::cout << "\x1b[31m Failed Final Test : ❌" <<std::endl;
-        std::cout << "\x1b[31m Valid Password" <<std::endl;
+        std::cout << "\x1b[31m Invalid Password" <<std::endl;
     }
     return 0;
 }
