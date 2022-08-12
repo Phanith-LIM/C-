c59012e193b657502a9056493535677e95350d2a
Phanith Lim <86724860+Phanith-LIM@users.noreply.github.com>
2022-08-12 14:02:16 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-for (int i = 0; i < password.length(); i++){
-                        if (isupper(password[i])){
-                            break;
-                        }else{
-                            isPass4 = false;
-                        }
-                    }
-                }
