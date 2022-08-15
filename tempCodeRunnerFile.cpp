5140df80ef90859ead60d71b86be60d24d5bd5cd
Phanith Lim <86724860+Phanith-LIM@users.noreply.github.com>
2022-08-15 10:35:42 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-                // if(Bullets[0] == 1) {
-                //     Bullets[0] = 0;
-                //     numDead[i]++;
-                //     if(Bullets[0] == 0) std::cout <<"\x1b[31m"<<Bullets[k] <<"\t";
-                // }else 
