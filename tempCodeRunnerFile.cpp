b0ced004c573cf2782ce85aa472a3c34883ee092
Phanith LIM <limphanith.office@gmail.com>
2022-08-15 10:34:04 +0700

    delete

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+                // if(Bullets[0] == 1) {
+                //     Bullets[0] = 0;
+                //     numDead[i]++;
+                //     if(Bullets[0] == 0) std::cout <<"\x1b[31m"<<Bullets[k] <<"\t";
+                // }else 
